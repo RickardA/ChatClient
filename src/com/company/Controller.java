ed4ffa21316b305732ad3aa85700270deca093f9
Mathias Permlid <mathias.permlid@gmail.com>
2019-02-18 18:25:16 +0100

    Added ListView of users in the GUI. (Added mock up users).

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -4,6 +4,7 @@ import com.company.ChatRooms.UserInputField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 
 import java.net.URL;
@@ -13,11 +14,17 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     @FXML
-    public TextArea inputbox, outputbox; // userbox
+    public TextArea inputbox, outputbox;
+    public ListView userbox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         //get and print user list
+        userbox.getItems().add("Sean");
+        userbox.getItems().add("Johan");
+        userbox.getItems().add("Rickard");
+        userbox.getItems().add("Rami");
+        userbox.getItems().add("Mathias");
         //get and print chat history
     }
 
