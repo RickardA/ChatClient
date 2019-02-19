4f0fa65af49e6ca3acc26fecf0b8529d57596330
Sean <sean.kisch@hotmail.com>
2019-02-19 10:13:24 +0100

    Task 2 and 7 is done. Client sends to server which chatroom the client is in. Message class contains channel ID so that the server can send the message to all the users that have the same ID

--- src/com/company/ChatRooms/ChatOutputField.java
+++ src/com/company/ChatRooms/ChatOutputField.java
@@ -41,7 +41,7 @@ public class ChatOutputField implements Runnable{
                 Platform.runLater(new Runnable() {
                     @Override public void run() {
                         for (int i = 0; i < messages.getMessagesList().size(); i++) {
-                            Main.controller.recieveMessage(messages.getMessagesList().get(i).getSenderID() + " " + messages.getMessagesList().get(i).getTimeStamp() + "   " + messages.getMessagesList().get(i).getMessage());
+                            Main.controller.recieveMessage(messages.getMessagesList().get(i).getSenderName() + " " + messages.getMessagesList().get(i).getTimeStamp() + "   " + messages.getMessagesList().get(i).getMessage());
                         }
 
                     }
