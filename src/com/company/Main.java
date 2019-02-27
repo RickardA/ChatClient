f40ae680c6298cb0e52ed8e2a96f3b99feaddc2c
Mathias Permlid <mathias.permlid@gmail.com>
2019-02-27 13:16:45 +0100

    merge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -14,8 +13,7 @@ public class Main extends Application {
     public static Parent chatWindowRoot;
     public static FXMLLoader loader1;
 
-    private AnchorPane anchorPane;
-
+    
 
 
     @Override
